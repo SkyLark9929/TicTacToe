7e732953c83aabab896b8a7afad7ca7b9108f8cb
SkySlark9929 <skyslark9929@gmail.com>
2024-10-21 21:44:40 +0300

    Add two players which can tic and tac
    Add player object, from which to instances of play
    ers are created. The player has a method to make
    his move.
    
    Add game flow object, which controls the game seq
    uence.

--- source.js
+++ source.js
@@ -1,8 +1,8 @@
 const GAME_BOARD = (function(){
-    let gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    let gameBoard = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     
-    function tickSquare(x, y){
-        gameBoard[y][x] = 1;
+    function tickSquare(x, y, mark){
+        gameBoard[y][x] = mark;
     };
 
     function displayBoard(){
@@ -12,9 +12,43 @@ const GAME_BOARD = (function(){
     };
 
     function resetBoard(){
-        gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        gameBoard = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     };
 
     return {tickSquare, displayBoard, resetBoard};
 }())
 
+const GAME = (function(){
+    let inProgress = true;
+
+    function startGame(player1, player2){
+        while(inProgress){
+            GAME_BOARD.displayBoard();
+            player1.makeTurn();
+            GAME_BOARD.displayBoard();
+            player2.makeTurn();
+        };
+    };
+    
+    return {startGame};
+}());
+
+function createPlayer(name, mark){
+    
+    function makeTurn(){
+        console.log(`Player ${name} is making a turn`);
+        let coordinates = prompt(`${name}! Input the x;y :`);
+        coordinates = coordinates.split(';');
+        let x = Number(coordinates[0]);
+        let y = Number(coordinates[1]);
+
+        GAME_BOARD.tickSquare(x, y, mark);
+    }
+
+    return {makeTurn};
+};
+
+const playerJohn = createPlayer('John', 'X');
+const playerElijah = createPlayer('Elijah', 'O');
+
+GAME.startGame(playerElijah, playerJohn);
